b2b2efea5a322e502157b451c5733b9cad6d0b0b
VEGETAxHERE <feelthepowerofvegeta@gmail.com>
2024-05-11 10:41:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,15 +13,15 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
-START_IMG = getenv("START_IMG", "https://telegra.ph/file/84819fc115cb0eff32b2b.jpg")
+PING_IMG = getenv("PING_IMG", "https://graph.org/file/58fedaeb29218bba4ae8c.jpg")
+START_IMG = getenv("START_IMG", "https://graph.org/file/58fedaeb29218bba4ae8c.jpg")
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/tbcbotschat")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/tbc_bots")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/RINxMUSIC_II")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/RINxMUSIC_II")
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5090817443").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6665460685").split()))
 
 
 FAILED = "https://te.legra.ph/file/4c896584b592593c00aa8.jpg"
